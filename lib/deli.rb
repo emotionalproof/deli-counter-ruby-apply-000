a9eedcbb45cc6073b063d7870e88e62d372cdcf4
Chase <chaserabenn@gmail.com>
2020-04-28 14:37:47 +0000

    Automatically backed up by Learn

--- lib/deli.rb
+++ lib/deli.rb
@@ -5,6 +5,7 @@ require_relative "../deli_counter"
 deli = ["Chase", "Bobby", "Jimmy", "Carley"]
 
 puts "Welcome to the Deli . . . Time to Nosh!"
+
 line(deli)
 
 puts "Would you like to take a number?"
@@ -15,3 +16,4 @@ if response.upcase == "Y"
   "Great! Please enter your name and then go to the back of the line"
   name = gets.strip.capitalize
   take_a_number(deli, name)
+end
