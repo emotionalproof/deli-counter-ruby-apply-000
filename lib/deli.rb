da09449c4e7ea6064f7b1e23a43c100c8958c9e8
Chase <chaserabenn@gmail.com>
2020-04-28 14:44:37 +0000

    Automatically backed up by Learn

--- lib/deli.rb
+++ lib/deli.rb
@@ -8,12 +8,18 @@ puts "Welcome to the Deli . . . Time to Nosh!"
 
 line(deli)
 
-puts "Would you like to take a number?"
-puts "Please enter 'Y' or 'N'"
-response = gets.strip
+until deli.empty?
+  puts "Would you like to take a number?"
+  puts "Please enter 'Y' or 'N'"
+  response = gets.strip
 
-if response.upcase == "Y"
-  "Great! Please enter your name and then go to the back of the line"
-  name = gets.strip.capitalize
-  take_a_number(deli, name)
+  if response.upcase == "Y"
+    "Great! Please enter your name and then go to the back of the line"
+    name = gets.strip.capitalize
+    take_a_number(deli, name)
+  else
+    puts "Ok. See you next time"
+  end
+
+  now_serving(deli)
 end
