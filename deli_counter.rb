0bc668782b818b1693843403323d659c8177a2a1
Chase <chaserabenn@gmail.com>
2020-04-28 14:19:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 katz_deli = []
 
-def line(deli) #def method "line" with parameter of "deli" which will be an array of people in line
+def line(deli) #method "line" with parameter of "deli" which will be an array of people in line
   if deli.empty? #setup a conditional based on if the line is empty
     puts "The line is currently empty." #if the conditional comes back true
   else #if the conditional comes back false
@@ -10,23 +10,27 @@ def line(deli) #def method "line" with parameter of "deli" which will be an arra
       #in order to start the indexing at 1
     puts "The line is currently:#{line_list.join("")}"
     #output a string and using string interpolation
-      #insert the joined array 
+      #insert the joined array
   end #close conditional
 end #close method
 
 
 
-def take_a_number(deli, person)
-  deli.push(person)
+def take_a_number(deli, person) #method with two parameters:
+    #the deli array and the name of person that will now enter the queue
+  deli.push(person) #push function to add the "person" to the end of the given deli queue
 
   puts "Welcome, #{person}. You are number #{deli.index(person) + 1} in line."
-end
+  #print a string and use interpolation to insert the person and the position
+  #the second interpolation uses the .index function to determine the index
+    #of that person
+end #close method
 
-def now_serving(deli)
-  if deli.empty?
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{deli[0]}."
-    deli.shift
-  end
-end
+def now_serving(deli) #method with one parameter
+  if deli.empty? #use .empty? function to determine if the array is empty
+    puts "There is nobody waiting to be served!" #if true, print
+  else #if the conditional comes back false
+    puts "Currently serving #{deli[0]}." #print a string and use interpolation to insert the person at the front of the line
+    deli.shift #use shift function to remove the person at the front
+  end #close conditional
+end #close method
