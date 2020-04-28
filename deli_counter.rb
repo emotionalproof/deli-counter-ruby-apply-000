ed634ba0ed4822b9940a69f90f35f7dc86081640
Chase <chaserabenn@gmail.com>
2020-04-28 14:04:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,33 +1,32 @@
 katz_deli = []
 
-def line(katz_deli)
-  if katz_deli.empty?
-    puts "The line is currently empty."
-  else
-    line_list = katz_deli.map.with_index { | person, i | " #{i + 1}. #{person}" }
+def line(deli) #def method "line" with parameter of "deli" which will be an array of people in line
+  if deli.empty? #setup a conditional based on if the line is empty
+    puts "The line is currently empty." #if the conditional comes back true
+  else #if the conditional comes back false
+    line_list = deli.map.with_index(1) { | person, i | " #{i}. #{person}" }
+    #map an array and assign to variable "line_list"
+    #use .map.with_index instead of map_with_index
+      #in order to start the indexing at 1
     puts "The line is currently:#{line_list.join("")}"
+    #output a string and using string interpolation
+      #insert the joined array 
+  end #close conditional
+end #close method
 
-    #katz_deli.each_with_index { | person, i | line_list << " #{i + 1}. #{person}"}
-    #output = "The line is currently:"
-    #line_list.each { |person| output += person }
-#
-    #puts output
-  end
+
+
+def take_a_number(deli, person)
+  deli.push(person)
+
+  puts "Welcome, #{person}. You are number #{deli.index(person) + 1} in line."
 end
 
-
-
-def take_a_number(katz_deli, person)
-  katz_deli.push(person)
-
-  puts "Welcome, #{person}. You are number #{katz_deli.index(person) + 1} in line."
-end
-
-def now_serving(katz_deli)
-  if katz_deli.empty?
+def now_serving(deli)
+  if deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift
+    puts "Currently serving #{deli[0]}."
+    deli.shift
   end
 end
