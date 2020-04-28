f578561cdd8e24e6d1b736d1390cfc15823e3fd7
Chase <chaserabenn@gmail.com>
2020-04-28 14:47:23 +0000

    Automatically backed up by Learn

--- lib/deli.rb
+++ lib/deli.rb
@@ -10,15 +10,18 @@ line(deli)
 
 until deli.empty?
   puts "Would you like to take a number?"
+  puts
   puts "Please enter 'Y' or 'N'"
   response = gets.strip
 
   if response.upcase == "Y"
     puts "Great! Please enter your name and then go to the back of the line"
     name = gets.strip.capitalize
+    puts
     take_a_number(deli, name)
   else
     puts "Ok. See you next time"
+    puts
   end
 
   now_serving(deli)
